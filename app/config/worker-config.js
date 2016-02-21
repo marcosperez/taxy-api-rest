5174a41eea522aaa4d5eaaed6291122b90b1a8a7
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2016-02-20 22:07:08 -0300

    agregada peticion post y la get devuelve los usuarios

--- app/config/worker-config.js
+++ app/config/worker-config.js
@@ -14,13 +14,28 @@ function configureWorker(application) {
 
 function configureApplication(application) {
   application.use(bodyParser.json());
-
+  application.use(bodyParser.urlencoded({ extended: true })); // for parsing       application/x-www-form-urlencoded
   application.use(function(req, res, next) {
-    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+   /* res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.set('Pragma', 'no-cache');
     res.set('Expires', '0');
     res.type('application/json');
+    next();*/
+     // Website you wish to allow to connect
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
+
   });
 }
 
--- app/controllers/v1/usuario/usuario-controller.js
+++ app/controllers/v1/usuario/usuario-controller.js
@@ -1,15 +1,55 @@
 
 function UsuarioController() {
+
     var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/taxis');
+    mongoose.connect('mongodb://localhost/taxis');
+
+    console.log("se conecto a la base de datos.");
 }
 
 function get(req, res, next) {
-  res.status(200).json({ hello: 'world' });
+  //
+
+    // Creación de variables para cargar el modelo
+
+    var Usuario = require('../../../models/usuario');
+
+
+
+    Usuario.find(function (err, users) {
+    if (err)
+        return console.error(err);
+        res.status(200).json(users);
+    })
+}
+
+function post(req,res,next){
+
+    // Creación de variables para cargar el modelo
+    console.log(req.body);
+    //console.log(res.json(req.body));;
+    console.log("Agregando usuario...."+req.body.nombre);
+    var Usuario = require('../../../models/usuario');
+
+    var user = new Usuario({
+                                    nombre: req.body.nombre,
+                                    categoria: req.body.categoria,
+                                    contraseña: req.body.contrasenia,
+                                    email: req.body.email
+                                });
+
+    user.save(function (err,user
+                        ) {
+                            if (err) return console.error(err);
+                            console.log("cargado usuario")
+                        }
+                );
+
 }
 
 UsuarioController.prototype = {
-  get: get
+    get: get,
+    post:post
 };
 
 var usuarioController = new UsuarioController();
