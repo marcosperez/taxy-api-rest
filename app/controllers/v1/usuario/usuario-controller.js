e3a2ef751090197a052ae5eb4d25bf2e8713b68b
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2016-02-28 12:37:41 -0300

    agregado signup

--- app/controllers/v1/usuario/usuario-controller.js
+++ app/controllers/v1/usuario/usuario-controller.js
@@ -1,4 +1,3 @@
-
 function UsuarioController() {
 
     var mongoose = require('mongoose');
@@ -8,35 +7,38 @@ function UsuarioController() {
     //console.log("se conecto a la base de datos.");
 }
 
-function post(req, res, next) {
+function login(req, res, next) {
     // Creación de variables para cargar el modelo
 
     var Usuario = require('../../../models/usuario');
-                console.log(req.body);
+    console.log(req.body);
     var UsuarioService = require('../../../services/usuario/usuario-service');
 
-    Usuario.findOne({nombre: req.body.nombre.toLowerCase()}, function(err, user) {
+    Usuario.findOne({
+        nombre: req.body.nombre.toLowerCase()
+    }, function (err, user) {
         // Comprobar si hay errores
 
         //error de autenticacion
-        if(err)
-             return res.status(401);
+        if (err)
+            return res.status(401);
 
         // Si el usuario existe o no
         // Y si la contraseña es correcta
-        console.log(req.body.password+" ==  "+user.contraseña);
-        if(user.contraseña == req.body.password)
-        {
+        console.log(req.body.password + " ==  " + user.contraseña);
+        if (user.contraseña == req.body.password) {
             //autenticacion correcta
-             console.log("Enviando token...");
-             return res.status(200).send({token: UsuarioService.createToken(user)});
+            console.log("Enviando token...");
+            return res.status(200).send({
+                token: UsuarioService.createToken(user)
+            });
         }
 
 
     });
 }
 
-function get(req,res,next){
+function signup(req, res, next) {
 
     // Creación de variables para cargar el modelo
     console.log(req.body);
@@ -45,28 +47,29 @@ function get(req,res,next){
 
     var Usuario = require('../../../models/usuario');
 
-    /*var user = new Usuario({
-                                    nombre: req.body.nombre,
-                                    categoria: req.body.categoria,
-                                    contraseña: req.body.contrasenia,
-                                    email: req.body.email
-                                });*/
+    var user = new Usuario({
+        nombre: req.body.nombre
+        , contraseña: req.body.contrasenia
+        , email: req.body.email
+    });
+
     var UsuarioService = require('../../../services/usuario/usuario-service');
-    user.save(function (err,user
-                        ) {
-                            if (err) return console.error(err);
-                            console.log("cargado usuario")
-                            return res
+
+    user.save(function (err, user) {
+        if (err) return console.error(err);
+        console.log("cargado usuario")
+        return res
             .status(200)
-            .send({token: UsuarioService.createToken(user)});
-                        }
-                );
+            .send({
+                token: UsuarioService.createToken(user)
+            });
+    });
 
 }
 
 UsuarioController.prototype = {
-    get: get,
-    post:post
+    get: get
+    , post: post
 };
 
 var usuarioController = new UsuarioController();
--- app/models/usuario.js
+++ app/models/usuario.js
@@ -3,10 +3,11 @@ var Schema       = mongoose.Schema;
 
 var UsuarioSchema   = new Schema({
     nombre: String,
-    categoria: String,
+    categoria: { type: String, default: 'user'},
     contraseña: String,
     email: String,
-    nivelAcceso: { type: Number, default: 0}
+    nivelAcceso: { type: Number, default: 0},
+    fechaCreacion:{type:Date , default:Date.now}
 });
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
