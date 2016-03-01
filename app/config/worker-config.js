b0307c0d8856aa09fe1acea574b2e8c73886128d
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2016-03-01 11:49:28 -0300

    arreglado

--- app/config/worker-config.js
+++ app/config/worker-config.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var express = require('express')
-        , cors = require('cors')
-;
+    , cors = require('cors');
 var application = express();
 var bodyParser = require('body-parser');
 var routeConfig = require('./route-config');
@@ -10,34 +9,36 @@ var settingsConfig = require('./settings/settings-config');
 var AuthMiddleware = require('../middleware/auth');
 
 function configureWorker(application) {
-  configureApplication(application);
-  configureRoutes(application);
+    configureApplication(application);
+    configureRoutes(application);
 
-  startServer(application);
+    startServer(application);
 }
 
 function configureApplication(application) {
-  application.use(bodyParser.json());
-  application.use(bodyParser.urlencoded({ extended: true })); // for parsing       application/x-www-form-urlencoded
+    application.use(bodyParser.json());
+
+    application.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
     application.use(cors());
-  application.use(AuthMiddleware.ensureAuthenticated);
-  application.use(function(req, res, next) {
-
-    next();
-
-  });
+    application.use(AuthMiddleware.ensureAuthenticated);
+    application.use(function (req, res, next) {
+        next();
+    });
 }
 
 function configureRoutes(application) {
-  routeConfig.registerRoutes(application);
+    routeConfig.registerRoutes(application);
 }
 
 function startServer(application) {
-  var server = http.createServer(application);
+    var server = http.createServer(application);
 
-  server.listen(settingsConfig.settings.workerPort, settingsConfig.settings.hostName, settingsConfig.settings.queueLength, function() {
-    console.log('listening at http://%s:%s', settingsConfig.settings.hostName, settingsConfig.settings.workerPort);
-  });
+    server.listen(settingsConfig.settings.workerPort, settingsConfig.settings.hostName, settingsConfig.settings.queueLength, function () {
+        console.log('listening at http://%s:%s', settingsConfig.settings.hostName, settingsConfig.settings.workerPort);
+    });
 }
 
 configureWorker(application);
